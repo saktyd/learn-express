f74976e359967498190054c3bb6ea4b580d8c5b6
Sakti Dewantoro <saktyd@gmail.com>
2019-02-19 15:44:41 +0700

    implement /users

--- index.js
+++ index.js
@@ -2,9 +2,43 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const users = [
+  {
+    id: 1,
+    name: 'Haidar'
+  },
+  {
+    id: 2,
+    name: 'Sakti'
+  },
+  {
+    id: 3,
+    name: 'Ajin'
+  },
+  {
+    id: 4,
+    name: 'Bara'
+  },
+  {
+    id: 5,
+    name: 'Mario'
+  },
+  {
+    id: 6,
+    name: 'Fahri'
+  }
+];
+
 app.get('/', (req, res) => {
   res.send({
     message: 'Hello World'
+  });
+});
+
+app.get('/users', (req, res) => {
+  res.send({
+    message: 'List of users',
+    data: users
   });
 });
 
